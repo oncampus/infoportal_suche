41f84cae405609c652e8a975f7b227129fd8c9dc
Daniel Waage <daniel.waage@oncampus.de>
2022-01-14 10:54:37 +0000

    Added a first structure

--- inforportal-api-suche.php
+++ inforportal-api-suche.php
@@ -1,2 +1,27 @@
 <?php
 
+/**
+* Plugin Name: Infoportal-api-search
+* Description: This is a plugin which will take other APIs and search them for content. It will only redirect to an extern side, shuld it find content.
+* Author: Laurenz Schindler, Daniel Waage
+* Version: 1.0.0
+*/
+
+// $objectToSave = ["a link", "another link"]
+
+/**
+ * Get the links from the DB
+ */
+function infoportal_api_suche_prefix() {
+    add_option("objectToSave", ["a link", "another link yet again"]);
+}
+
+/**
+ * Remove links from the DB
+ */
+function infoportal_api_suche_postfix() {
+    delete_option("objectToSave");
+}
+
+register_activation_hook(__FILE__, 'infoportal_api_suche_prefix');
+register_deactivation_hook(__FILE__, 'infoportal_api_suche_postfix');
