f00c77aa84564793a7a062217b9ba821bcec9187
Daniel Waage <root@laurenz.oncampus-server.de>
2022-01-31 09:02:57 +0000

    Added first REST-Call (Hardcoded)

--- inforportal-api-suche.php
+++ inforportal-api-suche.php
@@ -1,35 +1,121 @@
 <?php
 
 /**
-* Plugin Name: Infoportal-api-search
-* Description: This is a plugin which will take other APIs and search them for content. It will only redirect to an extern side, shuld it find content.
-* Author: Laurenz Schindler, Daniel Waage
-* Version: 1.0.0
-*/
-
-// $objectToSave = ["a link", "another link"]
-
-/**
- * Get the links from the DB
+ * Plugin Name: Infoportal-api-search
+ * Description: This is a plugin which will take other APIs and search them for content. It will only redirect to an extern side, shuld it find content.
+ * Author: Laurenz Schindler, Daniel Waage
+ * Version: 1.0.0
  */
-function infoportal_api_suche_prefix() {
-    add_option("objectToSave", ["a link", "another link yet again"]);
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 }
 
-/**
- * Remove links from the DB
- */
-function infoportal_api_suche_postfix() {
-    delete_option("objectToSave");
+// Encapsulating all functions is a good way to go
+class InfoportalApiSearch
+{
+    // Registering the function
+    function __construct()
+    {
+        add_action('admin_menu', array($this, 'settingsLink'));
+        add_action('admin_init', array($this, 'settings'));
+    }
+
+
+    function settings() {
+        add_settings_section('al_first_section', null, null, 'hit-that-lick');
+
+        add_settings_field('api_links', 'Choose your API', array($this, 'chooserHTML'), 'hit-that-lick', 'al_first_section');
+        register_setting('apiPluginGroup', 'al_links', array('sanatize_callback' => 'sanatize_text_field', 'default' => '0'));
+
+        add_settings_field('api_link_single', 'Link to REST-API', array($this, 'linkHTML'), 'hit-that-lick', 'al_first_section');
+        register_setting('apiPluginGroup', 'al_link_single', array('sanatize_callback' => 'sanatize_text_field', 'default' => 'linkle'));
+
+        add_settings_field('api_link_token', 'Generated Token', array($this, 'tokenHTML'), 'hit-that-lick', 'al_first_section');
+        register_setting('apiPluginGroup', 'al_link_token', array('sanatize_callback' => 'sanatize_text_field', 'default' => 'tonkle'));
+    }
+
+    function chooserHTML() { ?>
+        <select name="al_links">
+            <option value="0" <?php selected(get_option('al_links'), '0') ?>>Moodle API</option>
+            <option value="1" <?php selected(get_option('al_links'), '1') ?>>Wordpress API</option>
+        </select>
+    <?php }
+
+    function linkHTML() {?> 
+        <input type="text" name="al_link_single" value="<?php echo esc_attr(get_option('al_link_single')) ?>">
+    <?php }
+
+    function tokenHTML() {?> 
+        <input type="text" name="al_link_token" value="<?php echo esc_attr(get_option('al_link_token')) ?>">
+    <?php }
+
+    // Registering the Settings-Link
+    function settingsLink()
+    {
+        add_options_page('Devious Lick', 'Licks', 'manage_options', 'hit-that-lick', array($this, 'simpleHTML'));
+    }
+
+    function simpleHTML() { ?>
+        <div< class="wrap">
+            <h1>API Search Settings</h1>
+            <form action="options.php" method="POST">
+                <?php
+                    settings_fields('apiPluginGroup');
+                    do_settings_sections('hit-that-lick');
+                    submit_button();
+                ?>
+            </form>
+        </class>
+    <?php }
+
+    // Whatever happens here happens :)
+    function sickFunctionHTML()
+    {
+        $url = 'https://dev09.oncampus-server.de/webservice/rest/';
+
+        $testerToken = '6295a85a88bffcfe147c85c3a28beb96';
+        $wsFunction = 'core_course_search_courses';
+        $moodlewsRestFormat = 'json';
+        $criteriaName = 'search';
+        $criteriaValue = 'visual'; //What to look for. Obv shouldnt be hardcoded later on
+
+        // For a normal Course-Search query
+        $restCall = $url
+            . 'server.php?wstoken=' . $testerToken
+            . '&wsfunction=' . $wsFunction
+            . '&moodlewsrestformat=' . $moodlewsRestFormat
+            . '&criterianame=' . $criteriaName
+            . '&criteriavalue=' . $criteriaValue;
+
+        $response = file_get_contents($restCall);
+        $htmlStripped = strip_tags($response);
+        $jsonResponse = json_decode($htmlStripped, true);
+
+        /*
+        if ($htmlStripped) {
+            print_r($htmlStripped);
+        }
+
+        if ($jsonResponse) {
+            print_r($jsonResponse);
+        }
+        */
+
+        
+?>
+        <div class="wrap">
+            <h1>That Little Setting Stuff</h1>
+            <form action="options.php" method="post">
+                <?php
+                    settings_fields('al_links');
+                    do_settings_sections('hit-that-lick');
+                    submit_button();
+                ?>
+                <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e('Save'); ?>" />
+            </form>
+        </div>
+<?php }
 }
 
-/**
- * CleanUp on Uninstall
- */
-function infoportal_api_suche_ultima() {
-    delete_option("objectToSave");
-}
-
-register_activation_hook(__FILE__, 'infoportal_api_suche_prefix');
-register_deactivation_hook(__FILE__, 'infoportal_api_suche_postfix');
-register_uninstall_hook(__FILE__, 'infoportal_api_suche_ultima');
+$infoportalApiSearch = new InfoportalApiSearch();
